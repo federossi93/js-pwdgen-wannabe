f34ce865b04ff952ef9a338cce8a77235acf8363
federossi93 <rossifederico0993@gmail.com>
2022-10-05 14:08:30 +0200

    pwgeneration

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+// Chiedi all’utente il suo nome
+const firstName = prompt('Come ti chiami ?')
+
+// poi chiedi il suo cognome
+
+const lastName = prompt('Qual è il tuo cognome ? ')
+
+// poi chiedi il suo colore preferito
+
+const color = prompt('Qual è il tuo colore preferito ? ')
+
+// poi chiedi l'età
+
+const age = Number(prompt('Quanti anni hai ?'))
+
+// Infine scrivi sulla pagina nomecognomecolorepreferito22
+
+const password = document.getElementById("password").innerHTML = firstName + lastName + color + age
