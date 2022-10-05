490e0127a98ad97a5752a783e3c6314f744c39bd
federossi93 <rossifederico0993@gmail.com>
2022-10-05 16:42:33 +0200

    add ;

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,14 @@
 // Chiedi all’utente il suo nome
-const firstName = prompt('Come ti chiami ?')
+const firstName = prompt('Come ti chiami ?');
 
 // poi chiedi il suo cognome
-
-const lastName = prompt('Qual è il tuo cognome ? ')
+const lastName = prompt('Qual è il tuo cognome ? ');
 
 // poi chiedi il suo colore preferito
-
-const color = prompt('Qual è il tuo colore preferito ? ')
+const color = prompt('Qual è il tuo colore preferito ? ');
 
 // poi chiedi l'età
-
-const age = Number(prompt('Quanti anni hai ?'))
+const age = Number(prompt('Quanti anni hai ?'));
 
 // Infine scrivi sulla pagina nomecognomecolorepreferito22
-
-const password = document.getElementById("password").innerHTML = firstName + lastName + color + age
+document.getElementById("password").innerHTML = firstName + lastName + color + age;
